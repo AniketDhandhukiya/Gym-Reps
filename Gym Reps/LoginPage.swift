0a3cf2d92e92e08da7fc8f96767c186d358a2963
aniket dhandhukiya <addhandhukiya2309@gmail.com>
2023-07-20 15:51:55 +0530

    textfield

--- Gym Reps/LoginPage.swift
+++ Gym Reps/LoginPage.swift
@@ -9,13 +9,17 @@ import UIKit
 
 class LoginPage: UIViewController {
 
+    @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var TxtEmail: UITextField!
     @IBOutlet weak var Img: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        TxtEmail.layer.cornerRadius = 21
+        TxtEmail.layer.cornerRadius = 19
         TxtEmail.layer.borderWidth = 1
         TxtEmail.layer.borderColor = UIColor.white.cgColor
+        txtPassword.layer.cornerRadius = 19
+        txtPassword.layer.borderWidth = 1
+        txtPassword.layer.borderColor = UIColor.white.cgColor
         
     }
     
