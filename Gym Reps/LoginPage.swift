70a118f59a0bb203ed42d346608dd900e41b1efb
kirtanBaldha <kirtan9397@gmail.com>
2023-07-20 15:27:55 +0530

    Login Page

--- Gym Reps/LoginPage.swift
+++ Gym Reps/LoginPage.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class LoginPage: UIViewController {
 
+    @IBOutlet weak var Img: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        Img.image?.ciImage?.applyingGaussianBlur(sigma: 25.25)
         // Do any additional setup after loading the view.
     }
     
