cf94897da53ab1641797fb2052bac7bfe6a8baf8
aniket dhandhukiya <addhandhukiya2309@gmail.com>
2023-07-21 15:22:50 +0530

    photo change

--- Gym Reps/LoginPage.swift
+++ Gym Reps/LoginPage.swift
@@ -9,17 +9,23 @@ import UIKit
 
 class LoginPage: UIViewController {
 
+    @IBOutlet weak var bgview2: UIView!
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var lblLogin: UILabel!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var TxtEmail: UITextField!
     @IBOutlet weak var Img: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        TxtEmail.layer.cornerRadius = 19
-        TxtEmail.layer.borderWidth = 1
-        TxtEmail.layer.borderColor = UIColor.white.cgColor
-        txtPassword.layer.cornerRadius = 19
-        txtPassword.layer.borderWidth = 1
-        txtPassword.layer.borderColor = UIColor.white.cgColor
+        bgView.layer.cornerRadius = 15
+        bgView.layer.borderWidth = 1
+        bgView.layer.borderColor = UIColor.white.cgColor
+        bgview2.layer.cornerRadius = 15
+        bgview2.layer.borderWidth = 1
+        bgview2.layer.borderColor = UIColor.white.cgColor
+        lblLogin.layer.borderWidth = 1
+        //lblLogin.layer.borderColor = UIColor.sy.cgColor
+        lblLogin.layer.cornerRadius = 11
         
     }
     
